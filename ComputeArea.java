ba847cd8687aa65311a28289b6a9e85f75646632
althompsone <althompsone@gmail.com>
2016-03-24 16:48:05 +0000

    Update ComputeArea.java

--- ComputeArea.java
+++ ComputeArea.java
@@ -1,17 +1,1 @@
-//Just proposing addition of this comment to test the whole branch/change request thing!
-public class ComputeArea{
-	public static void main(String[] args) {
-		double radius; // Declare radius
-		double area; // Declare area
-
-		// Assign a radius
-		radius = 20;// New value is radius
-
-		// Compute area
-		area = radius*radius*3.14159;
-
-		// Display results
-		System.out.println("The area for the circle of radius" +
-		radius + "is" + area);
-	}
-   } 
+//hey!
